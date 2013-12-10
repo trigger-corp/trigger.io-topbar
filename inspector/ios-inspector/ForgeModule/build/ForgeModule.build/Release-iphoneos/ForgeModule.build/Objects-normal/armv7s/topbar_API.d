bd2f4094e489ec1718b5aa509fac4209dec11d55
Connor Dunn <connor@trigger.io>
2013-12-10 17:29:37 +0000

    Update .gitignore and remove ignored files.

--- inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphoneos/ForgeModule.build/Objects-normal/armv7s/topbar_API.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_API.m \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_API.h \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_Delegate.h \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_Util.h
--- inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphoneos/ForgeModule.build/Objects-normal/armv7s/topbar_EventListener.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_EventListener.m \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_EventListener.h \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_Delegate.h
