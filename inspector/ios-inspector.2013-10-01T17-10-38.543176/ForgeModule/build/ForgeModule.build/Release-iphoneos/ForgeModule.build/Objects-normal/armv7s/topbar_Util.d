337ca0b66ee99e7ccc6952107a4b9b949e570bad
Connor Dunn <connor@trigger.io>
2013-12-10 17:11:32 +0000

    Remove ignored files

--- inspector/ios-inspector.2013-10-01T17-10-38.543176/ForgeModule/build/ForgeModule.build/Release-iphoneos/ForgeModule.build/Objects-normal/armv7s/topbar_Util.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_Util.m \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_Util.h
--- inspector/ios-inspector.2013-10-01T17-10-38.543176/ForgeModule/build/ForgeModule.build/Release-iphonesimulator/ForgeModule.build/Objects-normal/i386/topbar_Delegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_Delegate.m \
-  /Users/james/Code/modules/topbar/inspector/ios-inspector/ForgeModule/topbar_Delegate.h
