7e4b487da01ae580bb7b5de1375cd89a6cbe5128
Antoine van Gelder <antoine@trigger.io>
2018-07-24 08:36:14 +0200

    Move responsibility for topbar life-cycle to ForgeViewController

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/topbar/EventListener.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/topbar/EventListener.java
@@ -1,50 +1,15 @@
 package io.trigger.forge.android.modules.topbar;
 
+import android.os.Bundle;
+
 import io.trigger.forge.android.core.ForgeApp;
 import io.trigger.forge.android.core.ForgeEventListener;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import io.trigger.forge.android.core.ForgeLog;
+import io.trigger.forge.android.core.ForgeViewController;
 
 public class EventListener extends ForgeEventListener {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		RelativeLayout topbar = new RelativeLayout(ForgeApp.getActivity());
-
-		int size = 50;
-		DisplayMetrics metrics = new DisplayMetrics();
-		ForgeApp.getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
-		int requiredSize = Math.round(metrics.density * size);
-
-		topbar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, requiredSize));
-		topbar.setGravity(Gravity.CENTER);
-
-		ColorDrawable bgColor = new ColorDrawable(0xFFEEEEEE);
-		topbar.setBackgroundDrawable(bgColor);
-
-		ForgeApp.getActivity().mainLayout.addView(topbar, 0);
-		ForgeApp.getActivity().mainLayout.setBackgroundColor(0xFFFFFFFF);
-
-		// Add default title
-		TextView title = new TextView(ForgeApp.getActivity());
-		if (ForgeApp.appConfig.has("name")) {
-			title.setText(ForgeApp.appConfig.get("name").getAsString());
-		}
-		title.setTextColor(0xFF000000);
-		title.setTextSize(TypedValue.COMPLEX_UNIT_PX, metrics.density * 24);
-		title.setGravity(Gravity.CENTER);
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(10000, 10000);
-		topbar.addView(title, params);
-
-		// Save references
-		Util.left = null;
-		Util.right = null;
-		Util.topbar = topbar;
-		Util.title = title;
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        ForgeViewController.setNavigationBarHidden(false, null);
+    }
 }
--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/topbar/Util.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/topbar/Util.java
@@ -1,56 +1,54 @@
 package io.trigger.forge.android.modules.topbar;
 
-import io.trigger.forge.android.core.ForgeApp;
-import io.trigger.forge.android.core.ForgeFile;
-import io.trigger.forge.android.util.BitmapUtil;
-
-import java.io.IOException;
-
-import com.google.gson.JsonElement;
-
 import android.app.Activity;
 import android.content.Context;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.Gravity;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.gson.JsonElement;
+
+import java.io.IOException;
+
+import io.trigger.forge.android.core.ForgeApp;
+import io.trigger.forge.android.core.ForgeFile;
+import io.trigger.forge.android.core.ForgeViewController;
+import io.trigger.forge.android.util.BitmapUtil;
+
 public class Util {
-	public static RelativeLayout topbar = null;
-	public static View title = null;
-	public static TextView titleText = null;
-	public static LinearLayout left = null;
-	public static LinearLayout right = null;
+    public static TextView titleText = null;
+    public static LinearLayout left  = null;
+    public static LinearLayout right = null;
 
-	public static void setTitle(Context context, String text) {
-		DisplayMetrics metrics = new DisplayMetrics();
-		((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
+    public static void setTitle(Context context, String text) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-		titleText = new TextView(context);
-		titleText.setText(text);
-		titleText.setTextColor(0xFF000000);
-		titleText.setTextSize(TypedValue.COMPLEX_UNIT_PX, metrics.density * 24);
-		titleText.setGravity(Gravity.CENTER);
+        titleText = new TextView(context);
+        titleText.setText(text);
+        titleText.setTextColor(0xFF000000);
+        titleText.setTextSize(TypedValue.COMPLEX_UNIT_PX, metrics.density * 24);
+        titleText.setGravity(Gravity.CENTER);
 
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(10000, 10000);
-		topbar.addView(titleText, params);
-		topbar.removeView(title);
-		title = titleText;
-	}
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(10000, 10000);
+        ForgeViewController.navigationBar.addView(titleText, params);
+        ForgeViewController.navigationBar.removeView(ForgeViewController.navigationBarTitle);
+        ForgeViewController.navigationBarTitle = titleText;
+    }
 
-	public static void setTitleImage(Context context, JsonElement icon) throws IOException {
-		ImageView newtitle = new ImageView(context);
-		newtitle.setScaleType(ImageView.ScaleType.CENTER);
-		
-		newtitle.setImageDrawable(BitmapUtil.scaledDrawableFromStream(context, new ForgeFile(ForgeApp.getActivity(), icon).fd().createInputStream(), 0, 50));
+    public static void setTitleImage(Context context, JsonElement icon) throws IOException {
+        ImageView newtitle = new ImageView(context);
+        newtitle.setScaleType(ImageView.ScaleType.CENTER);
 
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(10000, 10000);
-		topbar.addView(newtitle, params);
-		topbar.removeView(title);
-		title = newtitle;
-	}
+        newtitle.setImageDrawable(BitmapUtil.scaledDrawableFromStream(context, new ForgeFile(ForgeApp.getActivity(), icon).fd().createInputStream(), 0, 50));
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(10000, 10000);
+        ForgeViewController.navigationBar.addView(newtitle, params);
+        ForgeViewController.navigationBar.removeView(ForgeViewController.navigationBarTitle);
+        ForgeViewController.navigationBarTitle = newtitle;
+    }
 }
